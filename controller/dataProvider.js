07b2e99229f8c80e30a4a2853d1eff6448038e42
zhaoyou <you.zhao@i-md.com>
2012-03-23 00:06:24 +0800

    add add html page

--- controller/dataProvider.js
+++ controller/dataProvider.js
@@ -1,11 +1,21 @@
-var config = require('./../config')
+var config = require('./../config');
 
 // show things list.
 exports.list = function(req, res) {
-  config.mongo.db('test').collection('things').find().toArray(function(err, array) {
-     for(var i = 0; i < array.length; i++) {
-       console.log(array[i]);
-     }
-     res.render('index/list', {'title': 'List page show userInfo', 'things': array});
+  config.mongo.db('test').collection('user').find().toArray(function(err, array) {
+    res.render('index/list', {'title': 'List page show userInfo', 'users': array});
+  });
+};
+
+exports.goAdd = function(req, res) {
+  res.render('index/add', {'title': 'add User'});
+};
+
+exports.add = function(req, res) {
+  var user = req.body.user;
+  console.log('user: ' + user);
+  config.mongo.db('test').collection('user').save(user);
+  config.mongo.db('test').collection('user').find().toArray(function(err, array) {
+    res.render('index/list', {'title': 'List page show userInfo', 'users': array});
   });
 };
--- index.js
+++ index.js
@@ -1,24 +1,26 @@
-var config = require('./config')
-var dataProvider = require('./controller/dataProvider')
+var config = require('./config');
+var dataProvider = require('./controller/dataProvider');
 
 var app = config.express.createServer(
-  config.express.bodyParser(),    
-  config.express.logger()
-);
+    config.express.bodyParser(),
+    config.express.logger()
+    );
 
 app.use(config.express.static(__dirname + '/assets', {maxAge: 3600000 * 24 * 30}));
 
 app.set('view engine', 'html');
-app.set('views',  __dirname + '/views');
+app.set('views', __dirname + '/views');
 app.set('view options', {'layout': true});
 app.register('html', config.ejs);
 
 
-app.configure(function(){
+app.configure(function() {
   app.use(config.express.errorHandler({dumpExceptions: true, showStack: true}));
 });
 
-app.get('/list', dataProvider.list);
+app.get('/user', dataProvider.list);
+app.get('/user/add', dataProvider.goAdd);
+app.post('/user/add', dataProvider.add);
 app.get('/', dataProvider.list);
 
 console.log('server is start ' + config.hostname + ' listen ' + config.port);
